9ac8bb009d2faba7588c2c1fb0d89825d50a1935
Keginold Philip <keginoldp@hotmail.com>
2020-12-08 15:13:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,9 @@
 
 
 def position_taken?(board, index)
-  
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    board[index] == "X" || board[index] == "O"
+    true
 end
